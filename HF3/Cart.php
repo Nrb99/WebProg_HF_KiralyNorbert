43b2726c945e2e828df4bddaaf38fa0ed110b7c0
Nrb99 <kiraly204@gmail.com>
2021-11-22 18:46:38 +0200

    HF3

---
+++ HF3/Cart.php
@@ -0,0 +1,100 @@
+<?php
+
+
+class Cart
+{
+    /**
+     * @var CartItem[]
+     */
+    private array $items = [];
+
+    /**
+     * @return CartItem[]
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param CartItem[] $items
+     */
+    public function setItems(array $items): void
+    {
+        $this->items = $items;
+    }
+
+    // TODO Generate getters and setters of properties
+
+    /**
+     * Add Product $product into cart. If product already exists inside cart
+     * it must update quantity.
+     * This must create CartItem and return CartItem from method
+     * Bonus: $quantity must not become more than whatever
+     * is $availableQuantity of the Product
+     *
+     * @param Product $product
+     * @param int $quantity
+     * @return CartItem
+     */
+    public function addProduct(Product $product, int $quantity): CartItem
+    {
+        //TODO Implement method
+        foreach ($this->items as $ertekek){
+            if($ertekek->getProduct()===$product){
+                $ertekek->setQuantity($ertekek->getQuantity()+$quantity);
+                return $ertekek;
+            }
+
+        }
+        $cartItem=new CartItem($product,$quantity);
+        array_push($this->items,$cartItem);
+        return $cartItem;
+    }
+
+    /**
+     * Remove product from cart
+     *
+     * @param Product $product
+     */
+    public function removeProduct(Product $product)
+    {
+        //TODO Implement method
+       for($i=0;$i<count($this->items);$i++){
+            if($this->items[$i]->getProduct()===$product){
+                unset($this->items[$i]);
+
+            }
+       }
+    }
+
+
+    /**
+     * This returns total number of products added in cart
+     *
+     * @return int
+     */
+    public function getTotalQuantity(): int
+    {
+        //TODO Implement method
+        $total=0;
+        foreach ($this->items as $ertekek){
+            $total+=$ertekek->getQuantity();
+        }
+        return $total;
+    }
+
+    /**
+     * This returns total price of products added in cart
+     *
+     * @return float
+     */
+    public function getTotalSum(): float
+    {
+        $total=0;
+        foreach ($this->items as $ertek) {
+            $total+=$ertek->getProduct()->getPrice()*$ertek->getQuantity();
+        }
+        return $total;
+    }
+}
---
+++ HF3/Product.php
@@ -0,0 +1,113 @@
+<?php
+
+
+class Product
+{
+    private int $id;
+    private string $title;
+    private float $price;
+    private int $availableQuantity;
+
+    // TODO Generate constructor with all properties of the class
+
+    // TODO Generate getters and setters of properties
+    /**
+     * @param int $id
+     * @param string $title
+     * @param float $price
+     * @param int $availableQuantity
+     */
+    public function __construct(int $id, string $title, float $price, int $availableQuantity)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->availableQuantity = $availableQuantity;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAvailableQuantity(): int
+    {
+        return $this->availableQuantity;
+    }
+
+    /**
+     * @param int $availableQuantity
+     */
+    public function setAvailableQuantity(int $availableQuantity): void
+    {
+        $this->availableQuantity = $availableQuantity;
+    }
+
+
+
+    /**
+     * Add Product $product into cart. If product already exists inside cart
+     * it must update quantity.
+     * This must create CartItem and return CartItem from method
+     * Bonus: $quantity must not become more than whatever
+     * is $availableQuantity of the Product
+     *
+     * @param Cart $cart
+     * @param int $quantity
+     * @return CartItem
+     */
+    public function addToCart(Cart $cart, int $quantity): CartItem
+    {
+        foreach ($cart->getItems() as $ertek) {
+            if ($ertek->getProduct() === $this) {
+                $ertek->increaseQuantity();
+            }
+        }
+
+        return $cart->addProduct($this,$quantity);
+
+    }
+
+    /**
+     * Remove product from cart
+     *
+     * @param Cart $cart
+     */
+    public function removeFromCart(Cart $cart)
+    {
+        //TODO Implement method
+
+        $cart->removeProduct($this);
+
+    }
+}
